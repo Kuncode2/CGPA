f430291825793ea5007487be19ed2bf80db0d5f3
Kuncode2 <109961679+Kuncode2@users.noreply.github.com>
2022-08-09 22:09:39 +0530

    Add files via upload

---
+++ Cgpa.java
@@ -0,0 +1,16 @@
+public class Cgpa {
+public static void main(String args[])
+{
+double Maths,English,science,CGPA,CGPAper;
+Maths = 12;
+English = 24;
+science = 67; 
+CGPA = (Maths+English+science)/3;
+CGPAper = (float)(24*(CGPA)/100);
+System.out.println("CGPA Percentage is" +CGPAper);
+}
+}
+
+    
+
+
